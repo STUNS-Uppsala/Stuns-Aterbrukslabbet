46e430df77e8d063c43064026c0ca1a95101f9b7
Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
2024-02-26 12:21:54 +0100

    Move font imports to fonts.ts

---
+++ app/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Prompt } from "next/font/google";
+
+export const inter = Inter({ 
+    subsets: ["latin"] 
+});
+
+export const prompt = Prompt({ 
+    subsets: ["latin"], 
+    weight: ["600"] 
+});
