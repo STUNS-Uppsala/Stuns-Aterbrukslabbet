b3ec8703732f4b69b46f29e67a4b1b222f430e7e
Simon Clavensjö <simon@clavensjo.se>
2024-02-26 09:22:02 +0100

    move flex to start of classname

--- app/(main-page)/_components/navbar.tsx
+++ app/(main-page)/_components/navbar.tsx
@@ -2,10 +2,10 @@ import Logo from "@/components/logo";
 
 export default function Navbar() {
   return (
-    <header className="fixed top-0 w-full h-20 px-4 bg-gradient-to-b from-navbarStart to-secondary flex items-center">
-      <div className="max-w-[1920px] mx-auto flex items-center w-full justify-between">
+    <header className="flex fixed top-0 w-full h-20 px-4 bg-gradient-to-b from-navbarStart to-secondary items-center">
+      <div className="flex max-w-[1920px] mx-auto items-center w-full justify-between">
         <Logo />
-        <section className="space-x-4 md:block md:w-auto flex items-center justify-between w-full"></section>
+        <section className="flex space-x-4 md:block md:w-auto items-center justify-between w-full"></section>
       </div>
     </header>
   );
