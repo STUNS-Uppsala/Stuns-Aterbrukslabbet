3231fd08d1bdb8ef3f0408913a505fdcd72b8a6e
Simon Clavensjö <simon@clavensjo.se>
2024-02-27 09:11:28 +0100

    Remove unnecessary class in root layout
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("bg-background h-full", inter.className)}>{children}</body>
+      <body className={cn("bg-background", inter.className)}>{children}</body>
     </html>
   );
 }
