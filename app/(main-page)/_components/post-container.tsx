f0efba7dbf0745d3c688469272dad93de37ea1ff
Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
2024-03-01 13:16:46 +0100

    Clean up code
    Co-Authored-By: lukasgurra <142992155+lukasgurra@users.noreply.github.com>

--- app/(main-page)/_components/post-container.tsx
+++ app/(main-page)/_components/post-container.tsx
@@ -2,7 +2,7 @@ import Post from "./post";
 
 export default function PostContainer() {
   return (
-    <div className="flex flex-col gap-y-5 my-16">
+    <div className="flex flex-col md:gap-y-5 gap-y-3 my-16 md:px-5 px-2 mx-auto md:max-w-screen-md max-w-[360px]">
       <Post
         title={"Filterpapper 100 st"}
         description={
@@ -11,17 +11,17 @@ export default function PostContainer() {
         postType={"Erbjuds"}
         location={"Science Industries, Stenhagen"}
         creationDate={new Date(Date.now() - 20 * 24 * 60 * 60 * 1000)}
-        expirationDate={new Date(Date.now() - 365 * 24 * 60 * 60 * 1000)}
+        expirationDate={new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)}
       />
       <Post
-        title={"Filterpapper 100 st"}
+        title={"GlasKolvar 12 st"}
         description={
-          "En stor låda med 100 filterpapper av papper skänkes. De är i gått skick och helt oanvända."
+          "12 glaskolvar i utmärkt skick efterfrågas för ny kemilabb på Stunsta Universitet"
         }
         postType={"Efterfrågas"}
-        location={"Science Industries, Stenhagen"}
-        creationDate={new Date(Date.now() - 10 * 24 * 60 * 60 * 1000)}
-        expirationDate={null}
+        location={"Stunsta Universitet. Stenhagen"}
+        creationDate={new Date(Date.now() - 1 * 24 * 60 * 60 * 1000)}
+        expirationDate={new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)}
       />
     </div>
   );
