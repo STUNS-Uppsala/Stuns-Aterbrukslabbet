96728e46e6e8e43a64d756905bf41704f68e94e8
Simon Clavensjö <simon@clavensjo.se>
2024-03-06 14:57:43 +0100

    Make arrow function normal function
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>

--- utils/check-role.ts
+++ utils/check-role.ts
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs";
 import { Roles } from "@/types/globals";
 
+export function checkRole(role: Roles) {
   const { sessionClaims } = auth();
 
   return sessionClaims?.metadata.role === role;
