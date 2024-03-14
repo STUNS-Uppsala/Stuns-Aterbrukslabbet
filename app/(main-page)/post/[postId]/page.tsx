3c6d0c71f979bad1bbba7782d4ed0a4b35f3fc18
Simon Clavensjö <simon@clavensjo.se>
2024-03-14 14:10:51 +0100

    Add error for page
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
    Co-Authored-By: lukasgurra <142992155+lukasgurra@users.noreply.github.com>

--- app/(main-page)/post/[postId]/page.tsx
+++ app/(main-page)/post/[postId]/page.tsx
@@ -4,6 +4,7 @@ import GetPostColorAndExpirationText from "../../utils/get-post-color-and-expira
 import CreationDateToString from "../../utils/creation-date-to-string";
 import getNameAndEmailFromUserId from "../../utils/get-name-and-email-from-user-id";
 import Post from "../_components/post";
+import Link from "next/link";
 
 interface PostIdPageProps {
   params: {
@@ -14,7 +15,6 @@ interface PostIdPageProps {
 export default async function PostIdPage({ params }: PostIdPageProps) {
   const post = await getPostData(Number(params.postId));
   if (post) {
-    let disclaimerText = "";
     let creationDateString = CreationDateToString(post.createdAt);
     const { fullName, email } = await getNameAndEmailFromUserId(post.userId);
     const { postTypeColor, expirationDateText } = GetPostColorAndExpirationText(
@@ -22,14 +22,10 @@ export default async function PostIdPage({ params }: PostIdPageProps) {
         postType: post.postType,
       }
     );
-    if (post.postType === "Erbjuds") {
-      disclaimerText =
-        "Kontakt med människor sker på egen risk. Vi ansvarar inte för att människor agerar lagligt och inte lurar dig. Vi kan inte garantera produktens skick därför bör du verifiera med leverantören vad varan använts till innan.";
-    }
-    if (post.postType === "Efterfrågas") {
-      disclaimerText =
-        "Kontakt med människor sker på egen risk. Vi ansvarar inte för att människor agerar lagligt och inte lurar dig.";
-    }
+    const disclaimerText =
+      post.postType === "Erbjuds"
+        ? "Kontakt med människor sker på egen risk. Vi ansvarar inte för att människor agerar lagligt och inte lurar dig. Vi kan inte garantera produktens skick därför bör du verifiera med leverantören vad varan använts till innan."
+        : "Kontakt med människor sker på egen risk. Vi ansvarar inte för att människor agerar lagligt och inte lurar dig.";
     return (
       <Post
         post={post}
@@ -42,6 +38,22 @@ export default async function PostIdPage({ params }: PostIdPageProps) {
       />
     );
   } else {
-    return <p>Ingen post hittades</p>;
+    return (
+      <div className="flex w-full h-[52vh] items-end justify-center text-center">
+        <div className="flex flex-col max-w-screen-sm gap-y-2 px-3">
+          <h1 className="text-xl font-medium">
+            Oj då, inget inlägg hittades...
+          </h1>
+          <p className="text-pretty">
+            Detta inlägg verkar inte finnas. Om du tror att inlägget bör finnas
+            kontrollera då URL:en. Om du precis skapat inlägget kan det ta en
+            liten stund för inlägget att dyka upp.
+          </p>
+          <Link className="text-blue-600 hover:underline pt-1 text-lg" href="/">
+            Till startsidan
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
