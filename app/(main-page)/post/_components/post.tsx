fa412feca8661c3c91bba971359d30d644b47747
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-03-15 10:23:57 +0100

    Rename html elements

--- app/(main-page)/post/_components/post.tsx
+++ app/(main-page)/post/_components/post.tsx
@@ -26,24 +26,24 @@ export default function Post({ post, name, email }: PostProps) {
     });
 
   return (
-    <div className="my-5 md:pt-10 pt-3 md:pb-6 pb-4 md:max-w-screen-md max-w-[360px] bg-secondary rounded-2xl mx-auto">
+    <article className="mt-5 md:pt-10 pt-3 md:pb-6 pb-4 md:max-w-screen-md max-w-[360px] bg-secondary rounded-2xl mx-auto">
       {/* Post image should replace the div below */}
       <div className="aspect-[4/3] md:mt-0 mt-2 md:mx-24 mx-6 bg-primary rounded-md" />
       <div className="flex flex-col md:mx-16 mx-6 gap-y-1">
         <div className="flex pt-2 md:text-base text-xs justify-between">
-          <div className="flex gap-x-1 items-center">
+          <section className="flex gap-x-1 items-center">
             <MapPin className="md:block hidden shrink-0" size={16} />
             <MapPin className="md:hidden block shrink-0" size={12} />
             {post.location}
-          </div>
-          <div className="flex text-end gap-x-1 text-nowrap items-center">
+          </section>
+          <section className="flex text-end gap-x-1 text-nowrap items-center">
             <Clock className="md:block hidden" size={16} />
             <Clock className="md:hidden block" size={12} />
             {creationDateString}
-          </div>
+          </section>
         </div>
         <div className="flex md:text-base text-xs md:h-14 h-10 justify-between">
-          <div className="flex gap-x-1 h-1/2 items-center">
+          <section className="flex gap-x-1 h-1/2 items-center">
             <div
               className={cn(
                 "md:h-4 md:w-4 h-3 w-3 rounded-[50%]",
@@ -51,21 +51,21 @@ export default function Post({ post, name, email }: PostProps) {
               )}
             />
             {post.postType}
-          </div>
+          </section>
           {post.hasCustomExpirationDate && (
-            <div className="text-red-500 text-end">
+            <section className="text-red-500 text-end">
               <p>{expirationDateText}</p>
               <p>{post.expiresAt.toLocaleDateString("sv-SE")}</p>
-            </div>
+            </section>
           )}
         </div>
         <h1 className="md:text-3xl text-2xl">{post.title}</h1>
         <p className="md:text-base text-xs md:pt-2">{post.description}</p>
-        <div className="flex items-center mt-4">
+        <section className="flex items-center mt-4">
           <User className="md:block hidden" size={18} />
           <User className="md:hidden block" size={12} />
-          <p className="md:text-xl text-xs pl-1">{name}</p>
-        </div>
+          <p className="md:text-xl text-sm pl-1">{name}</p>
+        </section>
         <div className="flex justify-between items-center">
           <Dialog>
             <DialogTrigger>
@@ -90,6 +90,6 @@ export default function Post({ post, name, email }: PostProps) {
           </Dialog>
         </div>
       </div>
-    </div>
+    </article>
   );
 }
