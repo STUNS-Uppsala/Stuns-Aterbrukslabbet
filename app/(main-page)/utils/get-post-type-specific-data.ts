13d7b81b519e85782f8f07b5aa4d9f63d1f7d6db
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-03-15 14:48:07 +0100

    Fix minor error
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>

--- app/(main-page)/utils/get-post-type-specific-data.ts
+++ app/(main-page)/utils/get-post-type-specific-data.ts
@@ -10,7 +10,7 @@ export default function getPostTypeSpecificData({
   if (postType === "Erbjuds") {
     postTypeColor = "bg-offerColor";
     expirationDateText = "Hämta senast";
-    "Kontakt med människor sker på egen risk. Vi ansvarar inte för att människor agerar lagligt och inte lurar dig. Vi kan inte garantera produktens skick därför bör du verifiera med leverantören vad varan använts till innan."
+    disclaimerText = "Kontakt med människor sker på egen risk. Vi ansvarar inte för att människor agerar lagligt och inte lurar dig. Vi kan inte garantera produktens skick därför bör du verifiera med leverantören vad varan använts till innan.";
   } else if (postType === "Efterfrågas") {
     postTypeColor = "bg-requestColor";
     expirationDateText = "Vill ha senast";
