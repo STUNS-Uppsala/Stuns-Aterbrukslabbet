23793b8d5b778eab12d37f0320c47055d18a62e8
Simon Clavensjö <simon@clavensjo.se>
2024-03-18 10:12:27 +0100

    Console.log payload from webhook WIP

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,52 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env.local"
+    );
+  }
+
+  // Gets the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  const payload = await req.json();
+
+  // Creates a new Svix instance
+  const webhook = new Webhook(WEBHOOK_SECRET);
+
+  let event: WebhookEvent;
+
+  // Verifies the payload with the headers
+  try {
+    const payloadString = JSON.stringify(payload);
+    event = webhook.verify(payloadString, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (error) {
+    console.error("Error verifying webhook:", error);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+
+  if (event.type == "user.created") {
+    console.log(payload);
+  }
+  return new Response("", { status: 200 });
+}
