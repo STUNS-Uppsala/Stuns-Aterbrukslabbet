0652d991278795b8220b3a1fb5f845f6ada74601
Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
2024-03-18 10:59:40 +0100

    Adjust margin on main page
    Co-Authored-By: lukasgurra <142992155+lukasgurra@users.noreply.github.com>

--- app/(main-page)/_components/filter-container.tsx
+++ app/(main-page)/_components/filter-container.tsx
@@ -14,7 +14,7 @@ export default function FilterContainer({
   const labelText = `Sök bland ${totalPostCount} annonser`;
 
   return (
-    <div className="flex flex-col md:gap-y-3 gap-y-2 md:px-5 px-2 mx-auto md:max-w-screen-md max-w-[360px] mt-4">
+    <div className="flex flex-col md:gap-y-3 gap-y-2 mt-4  md:px-5 px-2 mx-auto md:max-w-screen-md max-w-[360px]">
       <SearchBar labelText={labelText} itemsFoundCount={postCount} />
       <PostTypeButtons />
     </div>
