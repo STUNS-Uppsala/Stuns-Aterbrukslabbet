876717bcde1c30363ec0f2a3aea5aaddef141cbe
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-03-22 09:21:20 +0100

    Add favicon and logo in navbar

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
+import logo from "@/public/images/logo.webp";
 import { prompt } from "@/app/fonts";
 
 export default function Logo() {
   return (
-    <Link href="/">
+    <Link href="/" className="flex gap-x-2 items-center">
+      <Image
+        src={logo}
+        alt="Website logo"
+        width={50}
+        height={50}
+      />
       <div className={cn("text-2xl", prompt.className)}>Återbrukslabbet</div>
     </Link>
   );
